794d0f54c92bc9345246011765f78218fc867fb7
Vinicius <vinicius.arruda40@hotmail.com>
2024-05-07 16:53:59 -0300

    updated file_manager.py

--- src/python/directory/file_manager.py
+++ src/python/directory/file_manager.py
@@ -1,3 +1,43 @@
+"""
+Module: file_manager.py
+
+This module provides a FileManager class for managing files and folders within a directory.
+
+Dependencies:
+    - datetime: Provides classes for manipulating dates and times.
+    - pathlib: Provides an object-oriented interface for working with file system paths.
+    - typing: Provides support for type hints.
+
+Usage Example:
+    from file_manager import FileManager
+
+    # Initialize FileManager with the current directory as the base path
+    file_manager = FileManager()
+
+    # Rename a file
+    file_manager.rename_file("old_file.txt", "new_file.txt")
+
+    # Move a file to a different location
+    file_manager.move_file("file.txt", "destination_folder")
+
+    # Rename multiple files using a mapping dictionary
+    path_mapping = {"old_file1.txt": "new_file1.txt", "old_file2.txt": "new_file2.txt"}
+    file_manager.rename_files(path_mapping)
+
+    # Move multiple files to different locations using a mapping dictionary
+    path_mapping = {"file1.txt": "destination_folder1", "file2.txt": "destination_folder2"}
+    file_manager.move_files(path_mapping)
+
+    # Exclude (delete) a file
+    file_manager.exclude_file("file_to_delete.txt")
+
+    # Exclude (delete) all files in a folder
+    file_manager.exclude_files_in_folder("folder_to_clear")
+
+    # Create a new folder if it does not already exist
+    created_folder_path = file_manager.create_folder("new_folder")
+"""
+
 from datetime import datetime
 from pathlib import Path
 from typing import Dict
