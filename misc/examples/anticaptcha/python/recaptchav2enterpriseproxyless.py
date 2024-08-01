7417db89423ba7e9999030b75a90910606d5f775
Vinicius <vinicius.arruda40@hotmail.com>
2024-08-01 15:40:23 -0300

    style: formatting changes

--- misc/examples/anticaptcha/python/recaptchav2enterpriseproxyless.py
+++ misc/examples/anticaptcha/python/recaptchav2enterpriseproxyless.py
@@ -9,34 +9,34 @@ class RecaptchaV2EnterpriseProxylessExample:
         self.recaptcha_site_key = 'your-site-recaptcha-api-key'
         self.driver = webdriver.Chrome()
         self.solve_captcha()
-        
+
     def solve_captcha(self) -> None:
         self.driver.get("https://your_site_with/recaptchav2/Enterprise/Proxyless.com")
-        
+
         login_element = self.driver.find_element(By.ID, "login-id")
         login_element.send_keys("my-login")
-        
+
         password_element = self.driver.find_element(By.ID, "password-id")
         password_element.send_keys("my-secret-password")
-        
+
         button_element = self.driver.find_element(By.ID, "button-id")
         button_element.click()
-        
+
         current_url: str = self.driver.current_url
-        
+
         solver = recaptchaV2EnterpriseProxyless()
         solver.set_verbose(1)
         solver.set_key(self.anticaptcha_api_key)
         solver.set_website_url(current_url)
         solver.set_website_key(self.recaptcha_site_key)
-        
+
         g_response = solver.solve_and_return_solution()
-        
+
         if g_response != 0:
             self.execute_recaptcha_callback(g_response=g_response)
         else:
             raise Exception(f"Anti-Captcha finished with error: {solver.error_code}")
-        
+
     def execute_recaptcha_callback(self, g_response: str):
         """JavaScript code to find the callback dynamically"""
         js_code = """
@@ -53,8 +53,8 @@ class RecaptchaV2EnterpriseProxylessExample:
         }
         return false;
         """
-        
+
         result = self.driver.execute_script(js_code, g_response)
-        
+
         if not result:
             raise Exception("Callback not found or executed.")
